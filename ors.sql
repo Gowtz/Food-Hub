85de6ae6b217b7e4114bcccade862ec753ed233a
Gowtham <gajavijay4@gmail.com>
2022-12-24 16:28:57 +0530

    sql

---
+++ ors.sql
@@ -0,0 +1,210 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Dec 24, 2022 at 11:57 AM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 7.4.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ors`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cust`
+--
+
+CREATE TABLE `cust` (
+  `cid` int(11) NOT NULL,
+  `cname` varchar(30) NOT NULL,
+  `usrname` varchar(30) NOT NULL,
+  `pass` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `cust`
+--
+
+INSERT INTO `cust` (`cid`, `cname`, `usrname`, `pass`, `email`) VALUES
+(12, 'TEST 1', 'CUST 1', '123', 'jztdjjzyaj@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `food`
+--
+
+CREATE TABLE `food` (
+  `fid` int(11) NOT NULL,
+  `fname` varchar(20) DEFAULT NULL,
+  `sid` int(11) DEFAULT NULL,
+  `food_img` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `food`
+--
+
+INSERT INTO `food` (`fid`, `fname`, `sid`, `food_img`) VALUES
+(1, '', 1, '2.jpeg'),
+(3, '', 1, 'download.jpeg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `store`
+--
+
+CREATE TABLE `store` (
+  `sid` int(11) NOT NULL,
+  `sname` varchar(30) DEFAULT NULL,
+  `address` varchar(50) DEFAULT NULL,
+  `img` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `store`
+--
+
+INSERT INTO `store` (`sid`, `sname`, `address`, `img`) VALUES
+(1, 'Chai Kings', 'MVJCE', 'ezgif-1-93097cdd87.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tabl`
+--
+
+CREATE TABLE `tabl` (
+  `tid` int(11) NOT NULL,
+  `tname` varchar(5) DEFAULT NULL,
+  `sid` int(11) DEFAULT NULL,
+  `tcap` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tabl`
+--
+
+INSERT INTO `tabl` (`tid`, `tname`, `sid`, `tcap`) VALUES
+(1, '1', 2, 5),
+(2, '2', 2, 5),
+(3, '3', 2, 5),
+(4, '4', 5, 2),
+(5, '5', 5, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `trans`
+--
+
+CREATE TABLE `trans` (
+  `tid` int(11) NOT NULL,
+  `cname` varchar(20) DEFAULT NULL,
+  `datt` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `trans`
+--
+
+INSERT INTO `trans` (`tid`, `cname`, `datt`) VALUES
+(3, 'login', '2022-12-23'),
+(3, 'CUST 1', '2022-12-25'),
+(4, 'login', '2022-12-23'),
+(4, 'CUST 1', '2022-12-24'),
+(4, 'CUST 1', '2022-12-29'),
+(4, 'login', '2022-12-30'),
+(5, 'CUST 1', '2022-12-29');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `uid` int(11) NOT NULL,
+  `fname` varchar(20) DEFAULT NULL,
+  `lname` varchar(20) DEFAULT NULL,
+  `email` varchar(20) DEFAULT NULL,
+  `usrname` varchar(20) DEFAULT NULL,
+  `pass` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`uid`, `fname`, `lname`, `email`, `usrname`, `pass`) VALUES
+(1, 'ad', 'min', 'admin@gmail.com', 'admin', '123');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cust`
+--
+ALTER TABLE `cust`
+  ADD PRIMARY KEY (`cid`);
+
+--
+-- Indexes for table `food`
+--
+ALTER TABLE `food`
+  ADD PRIMARY KEY (`fid`);
+
+--
+-- Indexes for table `store`
+--
+ALTER TABLE `store`
+  ADD PRIMARY KEY (`sid`);
+
+--
+-- Indexes for table `tabl`
+--
+ALTER TABLE `tabl`
+  ADD PRIMARY KEY (`tid`);
+
+--
+-- Indexes for table `trans`
+--
+ALTER TABLE `trans`
+  ADD PRIMARY KEY (`tid`,`datt`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`uid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cust`
+--
+ALTER TABLE `cust`
+  MODIFY `cid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
