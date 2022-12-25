8e22371a07b0f0dc32a56a9ff6791a744a34a75d
Gowtham <gajavijay4@gmail.com>
2022-12-25 13:09:47 +0530

    added login_user

--- database/ses.php
+++ database/ses.php
@@ -2,7 +2,8 @@
    include('db_connect.php');
    session_start();
    
-   
+   $user_check = $_SESSION['login_user'];
+
    $ses_sql = mysqli_query($conn,"select userid from admin_login where userid= '$user_check'");
    $name = $_SESSION['login_user'] ?? "Login";
 
